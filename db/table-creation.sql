9ed706365c288f9446f8e46f9767b002566cabbf
Roman Roth <rroth@bgs.ac.uk>
2020-01-24 10:35:11 +0000

    drop tables if they exists

--- db/table-creation.sql
+++ db/table-creation.sql
@@ -1,3 +1,8 @@
+
+
+
+
+DROP TABLE IF EXISTS fooditem;
 CREATE TABLE fooditem (
 	id                   integer primary key
 	, soil_class         text
@@ -11,9 +16,9 @@ CREATE TABLE fooditem (
 	, selenium           numeric(20,10)
 	, zinc               numeric(20,10)
 
-)
+);
 
-
+drop table if exists household;
 Create Table Household (
 	id                            integer primary key
 	, household_expenditure       integer
@@ -21,23 +26,33 @@ Create Table Household (
 	, Household_head_type_code    text
 	, household_size              integer
 	, location                    integer  -- foregin key to a GIS polygon, the closest to where the houeholis actually located
-)
+);
 
+
+drop table if exists location;
 Create table location (
 	id                     integer primary key
 	, Country              text
 	, Name                 text
 	, Geometry             text --change to geometry
-)
+);
 
+drop table if exists dic_soil_type;
 Create table dic_soil_type (
 	id                            integer primary key
-	, name
-)
-
-create table food_date_source_link (
+	, name                        text
+);
 
 
+drop table if exists food_data_source_link;
+create table food_data_source_link (
+	fooditem_id               integer NOT NULL
+	, date_source_id          integer NOT NULL
+	, PRIMARY KEY (fooditem_id, date_source_id)
+);
+
+
+drop table if exists data_source;
 Create table data_source (
 	id                            integer primary key
 	, name                        text
@@ -45,4 +60,4 @@ Create table data_source (
 	, publication                 text
 	, isbn                        text
 	, notes                       text
-)
+);

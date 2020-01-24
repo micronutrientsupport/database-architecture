479f9eaa430eb7f8f7bd4d47b47a2d67a8ccb83a
Roman Roth <rroth@bgs.ac.uk>
2020-01-24 15:58:50 +0000

    Moar SQL

--- db/table-creation.sql
+++ db/table-creation.sql
@@ -1,10 +1,15 @@
 
 
+DROP TABLE IF EXISTS FOOD_GROUP CASCADE;
+CREATE TABLE FOOD_GROUP (
+	id               integer primary key
+	, name           text
+);
 
-
-DROP TABLE IF EXISTS FOODITEM;
+DROP TABLE IF EXISTS FOODITEM CASCADE;
 CREATE TABLE FOODITEM (
 	id                   integer primary key
+	, food_group         integer REFERENCES FOOD_GROUP (id)
 	, soil_class         text
 	, energy             numeric(20,10)
 	, phytate            numeric(20,10)
@@ -15,46 +20,50 @@ CREATE TABLE FOODITEM (
 	, magnesium          numeric(20,10)
 	, selenium           numeric(20,10)
 	, zinc               numeric(20,10)
-
 );
 
-DROP TABLE IF EXISTS HOUSEHOLD;
+DROP TABLE IF EXISTS HOUSEHOLD CASCADE;
 CREATE TABLE HOUSEHOLD (
 	id                            integer primary key
 	, household_expenditure       integer
 	, location_type_code          text   --- urban, rural, unknown
 	, Household_head_type_code    text
 	, household_size              integer
-	, location                    integer  -- foregin key to a GIS polygon, the closest to where the houeholis actually located
+	, location                    integer  REFERENCES LOCATION (id)-- foreign key to a GIS polygon, the closest to where the households actually located
 );
 
 
-DROP TABLE IF EXISTS LOCATION;
+DROP TABLE IF EXISTS COUNTRY CASCADE; 
+CREATE TABLE COUNTRY ( 
+	id                            text primary key ---ISO 3166  two letter codes
+	, name                        text
+);
+
+
+DROP TABLE IF EXISTS LOCATION CASCADE;
 CREATE TABLE LOCATION (
 	id                     integer primary key
-	, Country              text
+	, Country              text NOT NULL REFERENCES country (id)
 	, Name                 text
 	, Geometry             text --change to geometry
 );
 
-DROP TABLE IF EXISTS DIC_SOIL_TYPE;
+DROP TABLE IF EXISTS DIC_SOIL_TYPE CASCADE;
 CREATE TABLE DIC_SOIL_TYPE (
 	id                            integer primary key
 	, name                        text
 );
 
 
-DROP TABLE IF EXISTS FOOD_DATA_SOURCE_LINK;
+DROP TABLE IF EXISTS FOOD_DATA_SOURCE_LINK CASCADE;
 CREATE TABLE FOOD_DATA_SOURCE_LINK (
-	fooditem_id               integer NOT NULL
-	, date_source_id          integer NOT NULL
+	fooditem_id               integer NOT NULL REFERENCES FOODITEM (id)
+	, date_source_id          integer NOT NULL REFERENCES DATA_SOURCE (id)
 	, PRIMARY KEY (fooditem_id, date_source_id)
-	, CONSTRAINT FOOD_DATA_SOURCE_LINK_FK1 FOREIGN KEY fooditem_id REFERENCES FOODITEM.id
-	, CONSTRAINT FOOD_DATA_SOURCE_LINK_FK2 FOREGIN KEY date_source_id REFERENCES DATA_SOURCE.id
 );
 
 
-DROP TABLE IF EXISTS DATA_SOURCE;
+DROP TABLE IF EXISTS DATA_SOURCE CASCADE;
 CREATE TABLE DATA_SOURCE (
 	id                            integer primary key
 	, name                        text
@@ -63,3 +72,5 @@ CREATE TABLE DATA_SOURCE (
 	, isbn                        text
 	, notes                       text
 );
+
+

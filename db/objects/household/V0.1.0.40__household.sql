99e03316c21aa3d10bcd274e108075e5d5b4a7c2
Roman Roth <rroth@bgs.ac.uk>
2020-10-13 16:40:04 +0100

    remove household head, add household wealth quintile
    gh-9

--- db/objects/household/V0.1.0.40__household.sql
+++ db/objects/household/V0.1.0.40__household.sql
@@ -1,8 +1,8 @@
 CREATE TABLE HOUSEHOLD (
 	id                            integer primary key
-	, household_expenditure       integer
+	, location                    integer  REFERENCES LOCATION (id)-- foreign key to a GIS point, the closest to where the households actually located
 	, location_type_code          text   --- urban, rural, unknown
-	, Household_head_type_code    text
 	, household_size              integer
-	, location                    integer  REFERENCES LOCATION (id)-- foreign key to a GIS polygon, the closest to where the households actually located
+	, household_expenditure       integer
+	, wealth_quintile             integer
 );

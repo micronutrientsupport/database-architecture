399e760b28cfc575efad98c0d6bb84847a881cd5
Roman Roth <rroth@bgs.ac.uk>
2020-10-14 13:22:31 +0100

    add more comments

--- db/objects/FCT_SOURCE/V0.1.0.26__FCT_SOURCE.sql
+++ db/objects/FCT_SOURCE/V0.1.0.26__FCT_SOURCE.sql
@@ -5,3 +5,5 @@ CREATE TABLE FCT_SOURCE (
 );
 INSERT INTO FCT_SOURCE (id, name) VALUES (1, 'Malawi');
 INSERT INTO FCT_SOURCE (id, name) VALUES (2, 'Gambia Prynand Paul');
+
+COMMENT ON TABLE FCT_SOURCE IS 'The names of the various Food Composition Tables from which we derive the information about food composition';

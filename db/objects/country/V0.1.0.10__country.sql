a747c3cd1bfd7a821c72beb38d5259690e8a508f
Roman Roth <rroth@bgs.ac.uk>
2020-10-22 15:43:40 +0100

    add geometry column to countries table

--- db/objects/country/V0.1.0.10__country.sql
+++ db/objects/country/V0.1.0.10__country.sql
@@ -1,5 +1,6 @@
 CREATE TABLE COUNTRY (
 	id        text primary key ---ISO 3166  two letter codes
 	, name    text
+	, geometry geometry(Polygon,4326)
 );
 COMMENT ON TABLE COUNTRY IS 'ISO 3166 list of countries';

9e257af3b211fe2615ca5ba4d142a7399a6a9d4f
Roman Roth <rroth@bgs.ac.uk>
2020-11-16 10:27:07 +0000

    Add simple polygon test data for country: Gambia

--- db/test-data/V0.2.0.600__location_test_data.sql
+++ db/test-data/V0.2.0.600__location_test_data.sql
@@ -1,1 +1,2 @@
 update country set geometry = GeomFromText('POLYGON ((32 -10, 34 -10, 36 -16, 35 -17, 32 -10))', 4326) where id = 'MWI';
+update country set geometry = GeomFromText('POLYGON ((-16.8 13.6, -13.7 13.7, -13.8 13.2, -16.8 13, -16.8 13.6))', 4326) where id = 'GMB'

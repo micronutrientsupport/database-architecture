16e37b99fbcb64e40d0c65002b85e35f73df53cd
Roman Roth <rroth@bgs.ac.uk>
2020-11-20 14:03:48 +0000

    countries can be disjoint polygons

--- db/objects/country/V0.1.0.10__country.sql
+++ db/objects/country/V0.1.0.10__country.sql
@@ -1,6 +1,6 @@
 CREATE TABLE COUNTRY (
 	id        text primary key ---ISO 3166  two letter codes
 	, name    text
-	, geometry geometry(Polygon,4326)
+	, geometry geometry(MultiPolygon,4326)
 );
 COMMENT ON TABLE COUNTRY IS 'ISO 3166 list of countries';

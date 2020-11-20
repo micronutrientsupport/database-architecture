87dae8ec9a650170a16b7de8c67e576dc0a4d62e
Roman Roth <rroth@bgs.ac.uk>
2020-11-20 16:41:14 +0000

    Add region column to country, region table

--- db/objects/country/V0.1.0.10__country.sql
+++ db/objects/country/V0.1.0.10__country.sql
@@ -1,6 +1,8 @@
 CREATE TABLE COUNTRY (
-	id        text primary key ---ISO 3166  two letter codes
-	, name    text
+	id         text primary key ---ISO 3166  two letter codes
+	, name     text
+	, region   integer  REFERENCES region (id)
 	, geometry geometry(MultiPolygon,4326)
 );
 COMMENT ON TABLE COUNTRY IS 'ISO 3166 list of countries';
+COMMENT on column country.region is 'geogrpahic regions as used by the World Health Organisation methodology Standard country or area codes for statistical use (M49). see https://unstats.un.org/unsd/methodology/m49/ and https://en.m.wikipedia.org/wiki/United_Nations_geoscheme_for_Africa';

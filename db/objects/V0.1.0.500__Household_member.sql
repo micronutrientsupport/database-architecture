047f55c366ed97f54b036539d84a62bcbea8cd0f
Roman Roth <rroth@bgs.ac.uk>
2020-11-24 15:13:23 +0000

    change names of boolean fields

--- db/objects/V0.1.0.500__Household_member.sql
+++ db/objects/V0.1.0.500__Household_member.sql
@@ -3,15 +3,15 @@ CREATE TABLE household_member (
     , household_id         integer   references household (id)
     , sex                  text CHECK (sex in ('m','f'))
     , age_in_months        integer
-    , breastfed_YN         boolean
-    , lactating_YN         boolean
-    , pregnant_YN          boolean
+    , is_breastfed         boolean
+    , is_lactating         boolean
+    , is_pregnant          boolean
 );
 COMMENT ON TABLE household_member IS 'The people living in a household and their biological characteristics';
 COMMENT ON COLUMN household_member.household_id     IS 'The ID number of the household that this person belongs to';
 COMMENT ON COLUMN household_member.sex              IS 'Whether the household member is male or female';
-COMMENT ON COLUMN household_member.age_in_months    IS $$The age of the person at the time of the survey. This will be recorded in months for children under 5 years, and in years for older children and adults. We convert it to months for all individuals$$;
-COMMENT on COLUMN household_member.breastfed_YN     IS 'Whether the child is being breastfed; "true" indicates that they do';
-COMMENT on COLUMN household_member.lactating_YN     IS 'Whether this woman is lactating; "true" indicates that she is';
-COMMENT on COLUMN household_member.pregnant_YN      IS 'Whether this woman is pregnant; "true" indicates that she is';
+COMMENT ON COLUMN household_member.age_in_months    IS 'The age of the person at the time of the survey, in months. Ages for individuals older than 5 years are likely to be calculated from the age in years';
+COMMENT on COLUMN household_member.is_breastfed     IS 'Whether the child is being breastfed; "true" indicates that they do';
+COMMENT on COLUMN household_member.is_lactating     IS 'Whether this woman is lactating; "true" indicates that she is';
+COMMENT on COLUMN household_member.is_pregnant      IS 'Whether this woman is pregnant; "true" indicates that she is';
 

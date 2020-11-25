6b49b9e8d5e627cce3684a99295654bcc76e6755
Bean, Andrew K <andan@bsg.ac.uk>
2020-11-25 11:50:36 +0000

    Add basic dictionary of micronutrients

---
+++ db/dictionary-data/V0.2.0.550__micronutrients-data.sql
@@ -0,0 +1,31 @@
+INSERT INTO
+    micronutrients (id, name, category )
+VALUES
+    (1, 'Vitamin A', 'Vitamin'),
+    (2, 'Vitamin B6', 'Vitamin'),
+    (3, 'Vitamin B12', 'Vitamin'),
+    (4, 'Vitamin C', 'Vitains'),
+    (5, 'Vitamin D', 'Vitamin'),
+    (6, 'Vitamin E', 'Vitamin'),
+    (7, 'Thiamin', 'Vitamin'),
+    (8, 'Riboflavin', 'Vitamin'),
+    (9, 'Folic Acid', 'Vitamin'),
+    (10, 'Pantothenate', 'Vitamin'),
+    (11, 'Biotin', 'Vitamin'),
+    (12, 'Phytic Acid', 'Vitamin'),
+
+    (13, 'Calcium (Ca)', 'Mineral'),
+    (14, 'Iron (Fe)', 'Mineral'),
+    (15, 'Magnesium (Mg)', 'Mineral'),
+    (16, 'Phosphorus (P)', 'Mineral'),
+    (17, 'Potassium (K)', 'Mineral'),
+    (18, 'Sodium (Na)', 'Mineral'),
+    (19, 'Zinc (Zn)', 'Mineral'),
+    (20, 'Iodine (I)', 'Mineral'),
+    (21, 'Selenium (Se)', 'Mineral'),
+
+    (22, 'Energy (kJ)', 'Other'),
+    (23, 'Protein', 'Other'),
+    (24, 'Fat', 'Other'),
+    (25, 'Carbohydrate', 'Other'),
+    (26, 'Fibre', 'Other')

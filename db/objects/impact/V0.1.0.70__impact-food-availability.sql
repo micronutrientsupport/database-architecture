a8d87c1e15aab2daf87e8e9b91357f2967282803
Bean, Andrew K <andan@bsg.ac.uk>
2021-01-22 16:14:48 +0000

    Update column headings, fix tabs/spaces

--- db/objects/impact/V0.1.0.70__impact-food-availability.sql
+++ db/objects/impact/V0.1.0.70__impact-food-availability.sql
@@ -1,29 +1,29 @@
 CREATE TABLE IMPACT_FOOD_AVAILABILITY (
-	ssp                 		text REFERENCES IMPACT_SCENARIO (ID)
-	, comdty            		text REFERENCES IMPACT_COMMODITY (ID)
-	, cty       				text REFERENCES COUNTRY (ID)
-	, yr          				smallint
-	, percapfoodavailability	numeric(20,10)
-	, calcium					numeric(20,10)
-	, folate					numeric(20,10)
-	, iron						numeric(20,10)
-	, magnesium					numeric(20,10)
-	, niacin					numeric(20,10)
-	, phosphorus				numeric(20,10)
-	, potassium					numeric(20,10)
-	, protein					numeric(20,10)
-	, riboflavin				numeric(20,10)
-	, thiamin					numeric(20,10)
-	, vit_a						numeric(20,10)
-	, vit_b6					numeric(20,10)
-	, vit_c						numeric(20,10)
-	, zinc						numeric(20,10)
+	scenario             text REFERENCES IMPACT_SCENARIO (ID)
+	, commodity          text REFERENCES IMPACT_COMMODITY (ID)
+	, country                   text REFERENCES COUNTRY (ID)
+	, year                      smallint
+	, percapfoodavailability    numeric(20,10)
+	, calcium                   numeric(20,10)
+	, folate                    numeric(20,10)
+	, iron                      numeric(20,10)
+	, magnesium                 numeric(20,10)
+	, niacin                    numeric(20,10)
+	, phosphorus                numeric(20,10)
+	, potassium                 numeric(20,10)
+	, protein                   numeric(20,10)
+	, riboflavin                numeric(20,10)
+	, thiamin                   numeric(20,10)
+	, vit_a                     numeric(20,10)
+	, vit_b6                    numeric(20,10)
+	, vit_c	                    numeric(20,10)
+	, zinc                      numeric(20,10)
 );
 COMMENT ON TABLE IMPACT_FOOD_AVAILABILITY IS 'Food availability and nutrient values reported by the IMPACT model';
-COMMENT on column impact_food_availability.comdty IS 'Commodity';
-COMMENT on column impact_food_availability.cty IS 'Country';
-COMMENT on column impact_food_availability.yr IS 'Year';
-COMMENT on column impact_food_availability.ssp IS 'Socioeconomic scenario';
+COMMENT on column impact_food_availability.commodity IS 'Commodity';
+COMMENT on column impact_food_availability.country IS 'Country';
+COMMENT on column impact_food_availability.year IS 'Year';
+COMMENT on column impact_food_availability.scenario IS 'Socioeconomic scenario';
 COMMENT on column impact_food_availability.percapfoodavailability IS 'Per capita food availability (kg, per person per year)';
 COMMENT on column impact_food_availability.calcium IS 'Calcium (mg, per person per year)';
 COMMENT on column impact_food_availability.folate IS 'Folate (mcg, per person per year)';

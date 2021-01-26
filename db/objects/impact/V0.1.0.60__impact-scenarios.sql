fee76d1fda62e4d0c5203c2852d1322f7193e795
Bean, Andrew K <andan@bsg.ac.uk>
2021-01-26 09:39:36 +0000

    chore: fix typo in column comment

--- db/objects/impact/V0.1.0.60__impact-scenarios.sql
+++ db/objects/impact/V0.1.0.60__impact-scenarios.sql
@@ -8,5 +8,5 @@ CREATE TABLE IMPACT_SCENARIO (
 COMMENT ON TABLE impact_scenario IS 'List of scenarios from the IMPACT model and their description';
 COMMENT on column impact_scenario.name is 'Scenario name';
 COMMENT on column impact_scenario.description is 'User facing summary of the scenario';
-COMMENT on column impact_scenario.descriptio is 'User facing brief summary of the scenario';
+COMMENT on column impact_scenario.brief_description is 'User facing brief summary of the scenario';
 COMMENT on column impact_scenario.is_baseline is 'Is this scenario used as a baseline for comparison';

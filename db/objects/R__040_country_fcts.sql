3db24b4c7341061a7554f6520eab565be098bd48
Bean, Andrew K <andan@bsg.ac.uk>
2021-03-03 11:24:37 +0000

    chore: rename country_fcts view for consistency

--- db/objects/R__040_country_fcts.sql
+++ db/objects/R__040_country_fcts.sql
@@ -1,8 +1,8 @@
-CREATE OR REPLACE VIEW country_fcts AS
+CREATE OR REPLACE VIEW composition_data_sources AS
 with fcts as (
     select 
-        fct_source.id as fct_id
-        , fct_source.name as fct_name
+        fct_source.id as composition_data_id
+        , fct_source.name as composition_data_name
         , country.id as country_id
         , ROW_NUMBER() over 
           (partition by country.id order BY			
@@ -12,6 +12,6 @@ with fcts as (
 
 from country left join fct_source on ST_COVERS(ST_ENVELOPE(fct_source.geometry), country.geometry)
 )
-select * from fcts where fcts.rank=1;
+select composition_data_id, composition_data_name, country_id from fcts where fcts.rank=1;
 
-COMMENT ON VIEW country_fcts IS 'View of simplified algorithm for "best" FCT for a given country';
+COMMENT ON VIEW composition_data_sources IS 'View of simplified algorithm for "best" FCT for a given country';

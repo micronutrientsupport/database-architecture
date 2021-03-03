c42f88ba6817ecd63a347cf36e012b3e443a0c84
Roman Roth <rroth@bgs.ac.uk>
2021-03-03 14:21:38 +0000

    fix column name and formatting

--- db/objects/impact/R__interpolate-impact-bau.sql
+++ db/objects/impact/R__interpolate-impact-bau.sql
@@ -1,15 +1,48 @@
 CREATE MATERIALIZED VIEW impact_summary as
 
-    with baseline as (
-    select id from impact_scenario),
-    impact_nutrients as (
-    select id, impact_column from micronutrient where in_impact = true),
-    threshold as (
-    select 107000 as threshold 
+with baseline as (
+    select
+        id
+    from
+        impact_scenario
+),
+impact_nutrients as (
+    select
+        id,
+        impact_column
+    from
+        micronutrient
+    where
+        is_in_impact = true
+),
+threshold as (
+    select
+        107000 as threshold
+)
+select
+    country.id as country,
+    impact_nutrients.id as micronutrient,
+    baseline.id as scenario,
+    threshold.threshold as target,
+    interpolate_impact_year.*,
+    - round(
+        (
+            (
+                threshold.threshold - interpolate_impact_year.reference_val
+            ) / threshold.threshold
+        ) * 100,
+        2
+    ) as difference
+from
+    country,
+    baseline,
+    impact_nutrients,
+    threshold,
+    interpolate_impact_year(
+        country.id,
+        baseline.id,
+        impact_nutrients.impact_column,
+        threshold.threshold
     )
-
-
-    select country.id as country, impact_nutrients.id as micronutrient, baseline.id as scenario, threshold.threshold as target, interpolate_impact_year.*, -round(((threshold.threshold-interpolate_impact_year.reference_val)/threshold.threshold)*100,2) as difference
-    from country, baseline, impact_nutrients, threshold, interpolate_impact_year(country.id,baseline.id,impact_nutrients.impact_column, threshold.threshold)
-
-    where interpolate_impact_year.intersect_year notnull 
+where
+    interpolate_impact_year.intersect_year notnull

b88a1054525eaa2644f21d970e947fc0bd21c252
Roman Roth <rroth@bgs.ac.uk>
2021-03-05 10:30:54 +0000

    ci: fix materialized view creation causing errors on blank db

--- db/afterVersioned__MaterializedViews.sql
+++ db/afterVersioned__MaterializedViews.sql
@@ -1,2 +1,11 @@
+
+-- Refresh materialized views in case new data has been added.
+-- We create dummy views to prevent errors when running the script from a
+-- blank database, as otherwise flyway will ty to refresh a non-existent
+-- materialized view. The dummy tables should get overwritten by the actual
+-- scripts for the materialized views
+
+CREATE  MATERIALIZED VIEW IF NOT EXISTS top20_mn_per_country AS SELECT 'x';
 REFRESH MATERIALIZED VIEW top20_mn_per_country;
+CREATE  MATERIALIZED VIEW IF NOT EXISTS top20_mn_per_hhsurvey AS SELECT 'x';
 REFRESH MATERIALIZED VIEW top20_mn_per_hhsurvey;

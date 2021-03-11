0646c1b9f5f8add25640c24f56d2c7f07602a2ef
Roman Roth <rroth@bgs.ac.uk>
2021-03-11 16:13:24 +0000

    feat: use non-hardcoded RDA value

--- db/objects/impact/R__033_impact_summary.sql
+++ db/objects/impact/R__033_impact_summary.sql
@@ -1,49 +1,30 @@
 DROP MATERIALIZED VIEW IF EXISTS impact_summary;
 CREATE MATERIALIZED VIEW impact_summary as
-
-with baseline as (
-    select
-        id
-    from
-        impact_scenario
-),
-impact_nutrients as (
-    select
-        id,
-        impact_column
-    from
-        micronutrient
-    where
-        is_in_impact = true
-),
-threshold as (
-    select
-        107000 as threshold
-)
-select
-    country.id as country,
-    impact_nutrients.id as micronutrient,
-    baseline.id as scenario,
-    threshold.threshold as target,
-    interpolate_impact_year.*,
-    - round(
-        (
-            (
-                threshold.threshold - interpolate_impact_year.reference_val
-            ) / threshold.threshold
-        ) * 100,
-        2
-    ) as difference
-from
-    country,
-    baseline,
-    impact_nutrients,
-    threshold,
-    interpolate_impact_year(
-        country.id,
-        baseline.id,
-        impact_nutrients.impact_column,
-        threshold.threshold
-    )
-where
-    interpolate_impact_year.intersect_year notnull
+SELECT
+	country.id as country
+    , micronutrient.id as micronutrient
+   	, impact_scenario.id as scenario
+    , intake_threshold.rda as recommeded
+    , interpolate_impact_year.reference_val / 365 as daily_reference_val
+    , interpolate_impact_year.reference_year
+    , interpolate_impact_year.intersect_year
+	, -1 * round(
+		(
+			(
+				intake_threshold.rda - (interpolate_impact_year.reference_val /365)
+			) / intake_threshold.rda
+		) * 100,
+		2
+	) as difference
+FROM
+	impact_scenario
+	CROSS JOIN country
+	CROSS JOIN micronutrient
+	JOIN intake_threshold on micronutrient.name = intake_threshold.nutrient_name
+	CROSS JOIN interpolate_impact_year(country.id, impact_scenario.id, micronutrient.impact_column,  intake_threshold.rda )
+WHERE
+micronutrient.is_in_impact = true
+AND sex = 'Female' AND age_upper < 30 AND age_lower > 24
+AND interpolate_impact_year.reference_val IS NOT NULL
+AND interpolate_impact_year.intersect_year IS NOT NULL
+;

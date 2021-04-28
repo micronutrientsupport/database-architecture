4f23ba5eee55d1ecb7347c824c33ccccbc447109
Andy Bevan <apbe@bgs.ac.uk>
2021-04-28 17:41:09 +0100

    Complete redesign of biomarker and group tables plus data

---
+++ db/dictionary-data/V0.2.0.440__survey.sql
@@ -0,0 +1,7 @@
+INSERT INTO
+survey (
+    id, name, publication_date, geometry
+)
+VALUES
+( '10',	'Test survey Dune Book 10', '2021-01-01', null),			
+( '20',	'Test survey Dune Book 20', '2021-06-01', null)

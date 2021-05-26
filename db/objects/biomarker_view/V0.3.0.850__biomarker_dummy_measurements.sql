1f9da43ab15a5c84fc320e0bfcf787c1fc48e28e
Bean, Andrew K <andan@bsg.ac.uk>
2021-05-26 11:38:38 +0100

    chore: update survey_id in dummy data

--- db/objects/biomarker_view/V0.3.0.850__biomarker_dummy_measurements.sql
+++ db/objects/biomarker_view/V0.3.0.850__biomarker_dummy_measurements.sql
@@ -1,3 +1,7 @@
+update household SET survey_id = 10 WHERE id = 1;
+update household SET survey_id = 10 WHERE id = 2;
+update household SET survey_id = 20 WHERE id = 3;
+
 insert into biomarker_measurement (id, household_member_id, zinc) VALUES(1, 1, 60.0);
 insert into biomarker_measurement (id, household_member_id, zinc) VALUES(2, 2, 72.0);
 insert into biomarker_measurement (id, household_member_id, zinc) VALUES(3, 3, 73.0);

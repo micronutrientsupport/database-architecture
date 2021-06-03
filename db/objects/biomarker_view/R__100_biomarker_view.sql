ea05f2b0b6a5599935b3bda23d3fe272eb55109d
Roman Roth <rroth@bgs.ac.uk>
2021-06-03 12:13:51 +0100

    fix column names in niomarker view

--- db/objects/biomarker_view/R__100_biomarker_view.sql
+++ db/objects/biomarker_view/R__100_biomarker_view.sql
@@ -1,6 +1,6 @@
 create or replace view biomarker_summary as
 
-select 
+select
     sgd.survey_id
     , sgd.group_id
     , sr.name as region_name
@@ -9,7 +9,7 @@ select
 	, hm.age_in_months
 	, hm.is_pregnant
 	, bm.was_fasting
-	, bm.am_or_pm
+	, bm.time_of_day_sampled
 	, hm.survey_cluster
 	, hm.survey_strata
 	, hm.survey_weight
@@ -17,8 +17,9 @@ select
 	, bm.ferritin
 	, bm.stfr
 	, bm.rbp
-	, bm.serum_retinol
+	, bm.retinol
 	, bm.rbc_folate
+	, bm.ps_folate
 	, bm.vitamin_b12
 	, bm.zinc
 	, bm.crp
@@ -37,7 +38,7 @@ join survey_group_def sgd on -- Details of the survey, groupings WRA, AGE etc.
 		-- If age bounds, match between bounds
 		-- and by sex.
 		-- TODO: check which of the bounds is inclusive and which exclusive
-		sgd.age_lower_in_months < hm.age_in_months 
+		sgd.age_lower_in_months < hm.age_in_months
 		and sgd.age_upper_in_months >= hm.age_in_months
 		and (( sgd.sex = hm.sex) or (sgd.sex = 'both'))
 		)
@@ -45,8 +46,8 @@ join survey_group_def sgd on -- Details of the survey, groupings WRA, AGE etc.
 		(
 		-- No age bounds,  still join in sex but not if PREG as
 		-- has extra constraints
-		sgd.age_lower_in_months isnull 
-		and sgd.age_upper_in_months isnull 
+		sgd.age_lower_in_months isnull
+		and sgd.age_upper_in_months isnull
 		and (( sgd.sex = hm.sex) or (sgd.sex = 'both'))
 		and sgd.group_id != 'PREG'
 		)
@@ -57,5 +58,5 @@ join survey_group_def sgd on -- Details of the survey, groupings WRA, AGE etc.
 		and hm.is_pregnant
 		)
 	)
-	
+
 	order by sgd.survey_id, sgd.group_id ASC

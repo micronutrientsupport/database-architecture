51aa1874e85f628754ff240321c41c996ae69f24
Bean, Andrew K <andan@bsg.ac.uk>
2021-06-08 15:35:29 +0100

    feat: Update scenario creation function to use micronutrient table ids

--- db/objects/R__087__scenarios.sql
+++ db/objects/R__087__scenarios.sql
@@ -4,9 +4,10 @@ returns setof fooditem as
 $$
 DECLARE
   schema text := current_schema();
+  mn_field text := (SELECT LOWER(fooditem_column) FROM micronutrient WHERE id=_field);
 BEGIN
 
-	return QUERY EXECUTE 
+	return QUERY EXECUTE
 	 'SELECT ' || replace(
 	 -- QUERY information schema table to get list of column names as a string seperated by comma
 	 array_to_string(ARRAY(SELECT c.column_name
@@ -15,13 +16,13 @@ BEGIN
            -- AND  c.column_name NOT IN('ca_in_mg')
     	), ',')
    		-- replace <field> with '<newvalue> as <field>' in query text
-    , '' || _field || '', '' || _new_value || '::numeric(20,10) as ' || _field || '') 
-   || ' FROM ' || schema || '.fooditem WHERE fct_source_id=' || _fct_source_id || ' AND food_genus_id=''' || _food_genus || '''
-    UNION SELECT * from ' || schema || '.fooditem WHERE fct_source_id=' || _fct_source_id || ' AND food_genus_id!=''' || _food_genus || ''''
+    , '' || mn_field || '', '' || _new_value || '::numeric(20,10) as ' || mn_field || '') 
+   || ' FROM "' || schema || '".fooditem WHERE fct_source_id=' || _fct_source_id || ' AND food_genus_id=''' || _food_genus || '''
+    UNION SELECT * from "' || schema || '".fooditem WHERE fct_source_id=' || _fct_source_id || ' AND food_genus_id!=''' || _food_genus || ''''
 	;
 
 END
 $$
 language plpgsql;
 
-select 'fn' as source, * from do_scenario(10, 'andy94', 'fe_in_mg', 5000)
+--select * from create_scenario_fct(10, 'andy94'::text, 'Ca'::text, 5000);

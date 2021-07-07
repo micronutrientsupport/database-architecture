f4c02e82efb32dfffd2628f7da933dfdc626692e
Roman Roth <rroth@bgs.ac.uk>
2021-07-07 12:12:54 +0100

    subregion table: add admin_level column and comments

--- db/objects/country/V0.1.0.11__subregion.sql
+++ db/objects/country/V0.1.0.11__subregion.sql
@@ -1,10 +1,19 @@
-CREATE TABLE Subregion (
-	id         text primary key ---ISO 3166  two letter codes
-	, name     text
-    , country  text REFERENCES country (id)
-	, type     text
+CREATE TABLE subregion (
+	id            text primary key ---ISO 3166  two letter codes
+	, name        text
+    , country     text REFERENCES country (id)
+	, type        text
+	, admin_level integer
+
 	, geometry geometry(MultiPolygon,4326)
 );
-COMMENT ON TABLE Subregion IS 'Level 1 subnational regions from GADM';
 
 CREATE INDEX subregion_geometry_idx ON Subregion USING GIST (geometry);
+
+
+COMMENT ON TABLE subregion              IS 'Level 1 subnational regions from GADM';
+COMMENT ON COLUMN subregion.name        IS 'The name of the admistrative area';
+COMMENT ON COLUMN subregion.country     IS 'The ISO 3166 alpha-2 code of the country to which this administrative region belongs';
+COMMENT ON COLUMN subregion.type        IS ''; -- TODO: explain this
+COMMENT ON COLUMN subregion.admin_level IS 'The hierarchichal level of this administrative subregion. 0 is the country itself, 1 is the first level of administrative subdivision, 2 is the second level of administrative subdivision, etc.';
+

de9308a046f498b731c2f81bfaf1413f98950bc3
Bean, Andrew K <andan@bsg.ac.uk>
2021-07-09 15:47:23 +0100

    chore: formatting improvements

--- db/objects/household/R__018_household_factor.sql
+++ db/objects/household/R__018_household_factor.sql
@@ -1,10 +1,41 @@
-create or replace view household_normalisation as
-
-with factors as (
--- AFE/AME for females
-select hm.household_id, sum(hnf.afe_f) as afe_factor, sum(hnf.ame_f) as ame_factor, count(hm.id) from household_member hm join household_normalisation_factor hnf on hm.age_in_months=hnf.age_in_months where hm.sex='f' group by hm.household_id, hm.sex  
-union
--- AFE/AME for males
-select hm.household_id, sum(hnf.afe_m) as afe_factor, sum(hnf.ame_f) as ame_factor, count(hm.id) from household_member hm join household_normalisation_factor hnf on hm.age_in_months=hnf.age_in_months where hm.sex='m' group by hm.household_id, hm.sex  
+create
+or replace view household_normalisation as with factors as (
+    -- AFE/AME for females
+    select
+        hm.household_id,
+        sum(hnf.afe_f) as afe_factor,
+        sum(hnf.ame_f) as ame_factor,
+        count(hm.id)
+    from
+        household_member hm
+        join household_normalisation_factor hnf on hm.age_in_months = hnf.age_in_months
+    where
+        hm.sex = 'f'
+    group by
+        hm.household_id,
+        hm.sex
+    union
+    -- AFE/AME for males
+    select
+        hm.household_id,
+        sum(hnf.afe_m) as afe_factor,
+        sum(hnf.ame_f) as ame_factor,
+        count(hm.id)
+    from
+        household_member hm
+        join household_normalisation_factor hnf on hm.age_in_months = hnf.age_in_months
+    where
+        hm.sex = 'm'
+    group by
+        hm.household_id,
+        hm.sex
 )
-select household_id, sum(afe_factor) as afe_factor, sum(ame_factor) as ame_factor, sum(count) as member_count from factors group by household_id
+select
+    household_id,
+    sum(afe_factor) as afe_factor,
+    sum(ame_factor) as ame_factor,
+    sum(count) as member_count
+from
+    factors
+group by
+    household_id

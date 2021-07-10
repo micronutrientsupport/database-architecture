2c65d97849de2941dbc520c9f3bfa8caf132fde7
Bean, Andrew K <andan@bsg.ac.uk>
2021-07-10 16:01:34 +0100

    feat: add median aggregator and create summary views aggregating household supply and deficiency across subregions

---
+++ db/objects/household/R__026_household_intake_pivot.sql
@@ -0,0 +1,59 @@
+create
+or replace view household_intake_deficiency_pivot as
+select
+    household_supply.*,
+    afe_ear_threshold.afe_ear,
+    (
+        household_supply.micronutrient_supply < afe_ear_threshold.afe_ear
+    ) as deficient
+from
+    (
+        SELECT
+            hin.survey_id,
+            hin.household_id,
+            hin.fct_source_id,
+            hin.subregion_id,
+            subregion_name,
+            mn.micronutrient_id,
+            mn.micronutrient_supply
+        FROM
+            household_intake_normalised hin
+            CROSS JOIN LATERAL (
+                VALUES
+                    ('A', VitaminA_in_RAE_in_mcg),
+                    ('B6', VitaminB6_in_mg),
+                    ('B12', VitaminB12_in_mcg),
+                    ('C', VitaminC_in_mg),
+                    ('D', VitaminD_in_mcg),
+                    ('E', VitaminE_in_mg),
+                    ('B1', Thiamin_in_mg),
+                    ('B2', Riboflavin_in_mg),
+                    ('B3', Niacin_in_mg),
+                    ('Folic Acid', Folicacid_in_mcg),
+                    ('B9', Folate_in_mcg),
+                    ('B5', Pantothenate_in_mg),
+                    ('B7', Biotin_in_mcg),
+                    ('IP6', PhyticAcid_in_mg),
+                    ('Ca', Ca_in_mg),
+                    ('Cu', Cu_in_mg),
+                    ('Fe', Fe_in_mg),
+                    ('Mg', Mg_in_mg),
+                    ('Mn', Mn_in_mcg),
+                    ('P', P_in_mg),
+                    ('K', K_in_mg),
+                    ('Na', Na_in_mg),
+                    ('Zn', Zn_in_mg),
+                    ('I', I_in_mcg),
+                    ('N', Nitrogen_in_g),
+                    ('Se', Se_in_mcg),
+                    ('Ash', Ash_in_g),
+                    ('Fibre', Fibre_in_g),
+                    ('Carbohydrates', carbohydrates_in_g),
+                    ('Cholesterol', Cholesterol_in_mg),
+                    ('Protein', TotalProtein_in_g),
+                    ('Fat', TotalFats_in_g),
+                    ('Energy', Energy_in_kCal),
+                    ('Moisture', Moisture_in_g)
+            ) AS mn("micronutrient_id", "micronutrient_supply")
+    ) as household_supply
+    join afe_ear_threshold on afe_ear_threshold.micronutrient_id = household_supply.micronutrient_id;
---
+++ db/objects/household/R__027_household_deficiency_aggregation.sql
@@ -0,0 +1,39 @@
+create
+or replace view household_deficiency_aggregation as
+select
+    survey_id,
+    fct_source_id,
+    subregion_id,
+    subregion_name,
+    ST_AsGeoJSON(geometry) as geometry,
+    micronutrient_id,
+    unit,
+    median(micronutrient_supply) as median_supply,
+    count(household_id) as household_count,
+    count(household_id) filter (
+        where
+            deficient
+    ) as deficient_count,
+    round(
+        (
+            (
+                count(household_id) filter (
+                    where
+                        deficient
+                )
+            ) :: numeric /(count(household_id))
+        ) * 100,
+        2
+    ) as deficient_percentage
+from
+    household_intake_deficiency_pivot hidp
+    join micronutrient m on hidp.micronutrient_id = m.id
+    join subregion s on s.id = hidp.subregion_id
+group by
+    survey_id,
+    fct_source_id,
+    subregion_id,
+    subregion_name,
+    geometry,
+    micronutrient_id,
+    m.unit

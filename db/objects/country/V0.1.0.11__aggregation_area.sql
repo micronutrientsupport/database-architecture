11e20c746cdc8214024482ec3363360afd1180b8
Roman Roth <rroth@bgs.ac.uk>
2021-07-13 11:45:17 +0100

    add comment

--- db/objects/country/V0.1.0.11__aggregation_area.sql
+++ db/objects/country/V0.1.0.11__aggregation_area.sql
@@ -16,6 +16,7 @@ COMMENT ON COLUMN aggregation_area.name          IS 'The name of the aggregation
 COMMENT ON COLUMN aggregation_area.country       IS 'The ISO 3166 alpha-3 code of the country to which this administrative region belongs, if applicable. Areas that are not defined by adminstrative boundaries will have this as NULL';
 COMMENT ON COLUMN aggregation_area.type          IS 'Whether this aggregation area is a: country, administrative area, or non-administrative area. Administrative areas are borders defined by their respctive country (e.g. districts) and form a hierarchical relationship with each other. Non-adminstrative area are indepedent of national borders, e.g. user-drawn polygons to select data, or geologic features.'; -- TODO: explain this
 COMMENT ON COLUMN aggregation_area.admin_level   IS 'The hierarchichal level of this administrative aggregation area, if this is an adminsitrative area. 0 is a country, 1 is the first level of administrative subdivision, 2 is the second level of administrative subdivision, etc. -1 if it is a combination of several countries (e.g.  https://unstats.un.org/unsd/methodology/m49/ and https://en.m.wikipedia.org/wiki/United_Nations_geoscheme_for_Africa). NULL for non-admin areas';
+COMMENT ON COLUMN aggregation_area.parent        IS 'The id of the administrative area containing this one. NULL for countries and for non-admin areas';
 
 
 

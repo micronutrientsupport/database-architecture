595eb3ddbf9fc8098fed64b25ded656b36f2bc3e
Roman Roth <rroth@bgs.ac.uk>
2021-07-13 10:36:52 +0100

    fix spelling

--- db/objects/country/V0.1.0.10__country.sql
+++ db/objects/country/V0.1.0.10__country.sql
@@ -5,4 +5,4 @@ CREATE TABLE COUNTRY (
 	, geometry geometry(MultiPolygon,4326)
 );
 COMMENT ON TABLE COUNTRY IS 'ISO 3166 list of countries';
-COMMENT on column country.region is 'geogrpahic regions as used by the World Health Organisation methodology Standard country or area codes for statistical use (M49). see https://unstats.un.org/unsd/methodology/m49/ and https://en.m.wikipedia.org/wiki/United_Nations_geoscheme_for_Africa';
+COMMENT on column country.region is 'geographic regions as used by the World Health Organisation methodology Standard country or area codes for statistical use (M49). see https://unstats.un.org/unsd/methodology/m49/ and https://en.m.wikipedia.org/wiki/United_Nations_geoscheme_for_Africa';

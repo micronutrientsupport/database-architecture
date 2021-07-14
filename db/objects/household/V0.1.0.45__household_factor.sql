6d5fe1299d83b46838acd8bb982dfc7c886e5514
Bean, Andrew K <andan@bsg.ac.uk>
2021-07-14 11:24:46 +0100

    chore: comment fix

--- db/objects/household/V0.1.0.45__household_factor.sql
+++ db/objects/household/V0.1.0.45__household_factor.sql
@@ -8,7 +8,6 @@ CREATE TABLE household_normalisation_factor (
 
 COMMENT ON TABLE household_normalisation_factor                       IS 'Factors to be applied to nutrient requirements given age and sex to normalise values across households';
 COMMENT on column household_normalisation_factor.age_in_months        IS 'Age of household member to apply adjustment for';
-COMMENT on column household_normalisation_factor.energy_requirement_m IS 'Energy requirement for a male of this age';
-COMMENT on column household_normalisation_factor.energy_requirement_f IS 'Energy requiement for a female of this age';
-COMMENT on column household_normalisation_factor.afe                  IS 'Adult Female Equivalent (AFE) adjustment factor for a person of this sex of this age';
-COMMENT on column household_normalisation_factor.ame                  IS 'Adult Male Equivalent (AME) adjustment factor for a person of this sex of this age';
+COMMENT on column household_normalisation_factor.energy_requirement   IS 'Energy requirement for a person of this sex and age';
+COMMENT on column household_normalisation_factor.afe                  IS 'Adult Female Equivalent (AFE) adjustment factor for a person of this sex and age';
+COMMENT on column household_normalisation_factor.ame                  IS 'Adult Male Equivalent (AME) adjustment factor for a person of this sex and age';

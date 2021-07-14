678d41c50ece9e7377b42faa3fd50589b76b2f75
Roman Roth <rroth@bgs.ac.uk>
2021-07-14 17:09:03 +0100

    remove nonsense surveys and survey group definitions

--- db/dictionary-data/V0.2.0.440__survey.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO
-survey (
-    id, name, publication_date, geometry
-)
-VALUES
-( '10',	'Test survey Dune Book 10', '2021-01-01', null),			
-( '20',	'Test survey Dune Book 20', '2021-06-01', null)

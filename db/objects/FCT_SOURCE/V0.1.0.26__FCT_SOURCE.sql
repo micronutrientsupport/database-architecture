8cf3eeccca58eb34a5583b883a9a0ae9844ed0a3
Roman Roth <rroth@bgs.ac.uk>
2021-07-15 14:06:35 +0100

    move FCT source to data repo

--- db/objects/FCT_SOURCE/V0.1.0.26__FCT_SOURCE.sql
+++ db/objects/FCT_SOURCE/V0.1.0.26__FCT_SOURCE.sql
@@ -5,7 +5,6 @@ CREATE TABLE FCT_SOURCE (
 	, publication_date            date
 	, notes                       text
 );
-INSERT INTO FCT_SOURCE (id, name, publication_date) VALUES (1, 'Malawi', '2019-01-01');
-INSERT INTO FCT_SOURCE (id, name) VALUES (2, 'Gambia Prynand Paul');
+
 
 COMMENT ON TABLE FCT_SOURCE IS 'The names of the various Food Composition Tables from which we derive the information about food composition';

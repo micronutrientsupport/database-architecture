56ec8ac8d915d895ce8ce98d5dde531b1f194910
Andy Bevan <apbe@bgs.ac.uk>
2021-07-15 09:25:57 +0100

    Add biomarker bad data view

---
+++ db/objects/Biomarker/V0.1.0.917__bad_biomarker_vals_view.sql
@@ -0,0 +1,46 @@
+
+create or replace view bad_biomarker_vals
+as
+select 
+filename,
+survey_name, 
+household_id, 
+person_id,
+case when ferritin::numeric > 999 then ferritin::text || ' GREATER THAN 999' else ferritin::text end as ferritin 
+,case when  stfr::numeric > 99 then stfr::text || ' GREATER THAN 999' else stfr::text end as stfr
+,case when  rbp::numeric > 99 then rbp::text || ' GREATER THAN 999' else rbp::text end as rbp
+,case when  retinol::numeric > 99 then retinol::text || ' GREATER THAN 999' else retinol::text end as retinol
+,case when  rbc_folate::numeric > 999 then rbc_folate::text || ' GREATER THAN 999' else rbc_folate::text end as rbc_folate
+,case when  ps_folate::numeric > 999 then ps_folate::text || ' GREATER THAN 999' else ps_folate::text end as ps_folate
+,case when  vitamin_b12::numeric > 9999 then vitamin_b12::text || ' GREATER THAN 999' else vitamin_b12::text end as vitamin_b12
+,case when  zinc::numeric > 999 then zinc::text || ' GREATER THAN 999' else zinc::text end as zinc
+,case when  crp::numeric > 999 then crp::text || ' GREATER THAN 999' else crp::text end as crp
+,case when  agp::numeric > 999 then agp::text || ' GREATER THAN 999' else agp::text end as agp
+,case when  iodine::numeric > 999 then iodine::text || ' GREATER THAN 999' else iodine::text end as iodine
+/*
+ , ferritin                numeric(5,2)
+    , stfr                    numeric(4,2)
+    , rbp                     numeric(4,2)
+    , retinol                 numeric(4,2)
+    , rbc_folate              numeric(5,1)
+    , ps_folate               numeric(3,1)
+    , vitamin_b12             numeric(5,1)
+    , zinc                    numeric(4,1)
+    , crp                     numeric(5,2)
+    , agp                     numeric(5,2)
+    , iodine                  numeric(5,2)
+*/
+from biomarker_import_vol
+where 
+ferritin::numeric > 999 or
+stfr::numeric > 99 or
+rbp::numeric > 99 or
+retinol::numeric > 99 or
+rbc_folate::numeric > 999 or
+ps_folate::numeric > 999 or
+vitamin_b12::numeric > 9999 or
+zinc::numeric > 999 or
+crp::numeric > 999 or
+agp::numeric > 999 or
+iodine::numeric > 999
+;

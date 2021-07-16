90ccfc2a53e6f2443393950eee63ba7eb00c9b99
Roman Roth <rroth@bgs.ac.uk>
2021-07-16 11:05:45 +0100

    fix typo

--- db/objects/household/V0.1.0.40__household.sql
+++ db/objects/household/V0.1.0.40__household.sql
@@ -13,7 +13,7 @@ CREATE TABLE household (
 );
 
 COMMENT ON TABLE Household IS 'A household is a grouping of people that live together and can be treated as a unit for certain pruposes. For example, we may have data on what foods a household as a whole consumed, rather than for individual people who live in that household';
-COMMENT on column household.original_id           IS 'The id of this household as hused in the original survey';
+COMMENT on column household.original_id           IS 'The id of this household as used in the original survey';
 COMMENT on column household.location              IS 'Geospatial point indicating where this household is located. Note that will not be accurate, due to privacy concerns. For some datasets, household locations will be clustered before being displaced (see e.g. 2.1.3.DHS geographic data in https://dhsprogram.com/pubs/pdf/SAR10/SAR10.pdf';
 COMMENT on column household.urbanity              IS 'Whether this household is located in an urban or rural area';
 COMMENT on column household.household_expenditure IS 'How much money this household spends per month, in local currency';

ea4dbed98e198eb3e20281812d7daef51ec8cc13
Bean, Andrew K <andan@bsg.ac.uk>
2021-08-29 16:25:56 +0100

    feat: Add lookup table between biomarker names and micronutrient ids

---
+++ db/objects/Biomarker/V0.1.0.650__biomarker_mn_lookup.sql
@@ -0,0 +1,21 @@
+CREATE TABLE biomarker_micronutrient_mapping (
+	biomarker_name               text primary key
+    , micronutrient_id           text REFERENCES micronutrient (id);
+
+COMMENT ON TABLE  biomarker_name IS 'Name of the biomarker';
+COMMENT ON COLUMN micronutrient_id IS 'ID of the corresponding micronutrient from the micronutrients table';
+
+INSERT INTO biomarker_micronutrient_mapping (biomarker_name, micronutrient_id)
+VALUES
+ ('haemoglobin'), 'Fe')
+,('ferritin', 'Fe' )
+,('stfr', 'Fe' )
+,('rbp', null )
+,('retinol'), null )
+,('rbc_folate', 'B9' )
+,('ps_folate', 'B9' )
+,('vitamin_b12', 'B12' )
+,('zinc', 'Zn' )
+,('crp', null )
+,('agp', null )
+,('iodine', 'I' );

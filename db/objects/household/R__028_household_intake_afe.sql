15415f6746ae4466069f209ace2226e3a5d4dc35
Bean, Andrew K <andan@bsg.ac.uk>
2021-09-02 11:25:19 +0100

    feat: add view of household aer values

---
+++ db/objects/household/R__028_household_intake_afe.sql
@@ -0,0 +1,9 @@
+create materialized view household_intake_afe as
+select 
+    fct_source_id as composition_data_id,
+    survey_id as consumption_data_id,
+    micronutrient_id,
+	micronutrient_supply as dietary_supply,
+	is_deficient,
+	afe_ear as deficient_value,
+from household_intake_afe_deficiency_pivot hiadp 

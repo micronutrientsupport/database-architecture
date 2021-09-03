a1b38fc0dea55c9f6d772461b62760e59eb9aa4c
Andy Bevan <apbe@bgs.ac.uk>
2021-09-03 15:13:43 +0100

    Refine source_file table

--- db/objects/V0.1.0.016__source_file.sql
+++ db/objects/V0.1.0.016__source_file.sql
@@ -7,4 +7,5 @@ CREATE TABLE source_file (
 COMMENT ON TABLE source_file                 IS 'The source file from which data has been loaded or imported into the database. ';
 COMMENT ON COLUMN source_file.file_name      IS 'The name of the file including file type/extension';
 COMMENT ON COLUMN source_file.created_by     IS 'Name of the person or organisation responsible for creating the file';
-COMMENT ON COLUMN source_file.notes          IS 'Notes about file prearation, considerations, assumptions etc. might include notes about which data were excluded from the file, and why';
+COMMENT ON COLUMN source_file.created_date   IS 'Date when file created';
+COMMENT ON COLUMN source_file.notes          IS 'Notes about file preparation, considerations, assumptions etc. might include notes about which data were excluded from the file, and why';

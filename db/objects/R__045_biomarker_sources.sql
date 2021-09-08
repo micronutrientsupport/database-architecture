4bb0d5c0797de9f7a707e002a7be7409dc91c087
Bean, Andrew K <andan@bsg.ac.uk>
2021-09-08 10:31:33 +0100

    feat: add view of age-gender groups per biomarker survey and add to biomarker data sources view

--- db/objects/R__045_biomarker_sources.sql
+++ db/objects/R__045_biomarker_sources.sql
@@ -3,6 +3,7 @@ with surveys as (
     select 
         survey.id as survey_id
         , the_biomarker_name as biomarker_name
+        , the_biomarker_groups as group_id
         , micronutrient_id
         , survey.name as survey_name
         , survey.description as survey_description
@@ -17,11 +18,13 @@ with surveys as (
 
 from country join survey on ST_COVERS(ST_ENVELOPE(survey.geometry), country.geometry)
 join survey_reported_biomarkers srb on survey.id=srb.survey_id 
+join survey_reported_groups srg on survey.id=srg.survey_id
 CROSS  JOIN unnest (srb.reported_biomarkers) as the_biomarker_name
+CROSS  JOIN unnest (srg.reported_groups) as the_biomarker_groups
 join biomarker_micronutrient_mapping bmm on the_biomarker_name=bmm.biomarker_name
 where survey.survey_type = 'biomarker' 
 )
-select country_id, biomarker_name, micronutrient_id, survey_id, survey_name, survey_description, survey_metadata_id from surveys
+select country_id, biomarker_name, group_id, micronutrient_id, survey_id, survey_name, survey_description, survey_metadata_id from surveys
 where rank = 1;
 
 COMMENT ON VIEW biomarker_data_sources IS 'View of simplified algorithm for "best" biomarker data source for a given country';

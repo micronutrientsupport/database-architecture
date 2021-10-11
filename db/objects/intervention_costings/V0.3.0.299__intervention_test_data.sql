2391af20881336dd5ccb35b60f51fa74c044a509
Roman Roth <rroth@bgs.ac.uk>
2021-10-11 17:06:50 +0100

    add test data for various factors

--- db/objects/intervention_costings/V0.3.0.299__intervention_test_data.sql
+++ db/objects/intervention_costings/V0.3.0.299__intervention_test_data.sql
@@ -4,10 +4,14 @@ INSERT INTO app_user (id, username) VALUES (1, 'testuser1');
 INSERT INTO intervention_source (id, short_text, citation_text) VALUES (1, 'GFDx', 'Global Fortification Data Exchange. Dashboard: Country Fortification. https://fortificationdata.org/country-fortification-dashboard/?alpha3_code=MWI&lang=en');
 
 insert into food_vehicle (id, vehicle_name) VALUES (1, 'wheat flour');
+insert into food_vehicle (id, vehicle_name) VALUES (2, 'oil');
 
 INSERT INTO intervention_type (id, name) VALUES (1, 'Industrial Fortification');
 INSERT INTO intervention_type (id, name) VALUES (2, 'Biofortification');
 INSERT INTO intervention_type (id, name) VALUES (3, 'Agro-Fortification');
+
+
+
 
 INSERT INTO program_status (id, description) VALUES (1, 'voluntary, transitioning to mandatory');
 
@@ -69,3 +73,81 @@ VALUES
 ('A'  , 1, 0),
 ('B12', 1, 0)
 ;
+
+insert into display_heading (id, display_heading_text) VALUES (1, 'Industry Information');
+insert into display_heading (id, display_heading_text) VALUES (2, 'Industry Startup Scale up cost summary');
+
+
+insert into display_sub_heading (id, display_sub_heading_text) VALUES (1, 'Equipment');
+
+INSERT INTO intervention_factor (
+    id
+    , food_vehicle_id
+    , label
+    , display_heading_id
+    , display_sub_heading_id
+    , formula_index
+)
+VALUES (
+    1
+    , 1 --wheat flour
+    , 'Number of domestic industrial Mills (capacity > 20 metric ton /day)'
+    , 1
+    , NULL
+    , 1
+),
+(
+    2
+    , 1 --wheat flour
+    , 'Imputed number of domestic mills fortifying food vehicle'
+    , 1
+    , NULL
+    , 2
+),
+(
+    3
+    , 2 --oil
+    , 'Number of domestic industrial edible oil refineries (capacity of > 5 MT/day)'
+    , 1
+    , NULL
+    , 1
+),
+(
+    4
+    , 2 --oil
+    , 'Imputed number of domestic industrial refineries fortifying food vehicle'
+    , 1
+    , NULL
+    , 2
+),
+(
+    5
+    , 1 --wheat flour
+    , 'Feeder/dosifier (unit cost) and installation'
+    , 1
+    , NULL
+    ,37
+),
+(
+    6
+    , 2 -- oil
+    , 'Stainless steel mixing tank (unit cost)'
+    , 1
+    , 1
+    , 37
+)
+;
+
+INSERT INTO factor_value (
+    intervention_id
+    , intervention_factor_id
+    , value
+    , year
+)
+VALUES
+(
+    1
+    , 1
+    , 3
+    , 2021
+)

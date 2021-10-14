aa4490719d44b650840b8837291dc262ac4799f6
Roman Roth <rroth@bgs.ac.uk>
2021-10-14 09:58:57 +0100

    add detail to intervention maturity

--- db/objects/intervention_costings/V0.1.0.219__intervention_maturity.sql
+++ db/objects/intervention_costings/V0.1.0.219__intervention_maturity.sql
@@ -1,9 +1,11 @@
 create table intervention_maturity (
-    id     integer CREATED BY DEFAULT AS IDENTITY
-    , intervention_id INTEGER REFERENCES intervention(id)
-    , YEAR      integer
-    , maturity    text CHECK IN ('SU', 'SC', 'BAU')
+    id                integer CREATED BY DEFAULT AS IDENTITY
+    , intervention_id integer REFERENCES intervention(id)
+    , year            integer
+    , maturity        text CHECK IN ('SU', 'SC', 'BAU')
 );
 
-comment on table intervention_maturity iS 'what the maturity of the intervention is; whether it is in the startup phase, being scaled up, or already operating and contuning business as ususal ';
-comment on column intervention_maturity.maturity iS 'SU = Start Up. SC = Scale Up. BAU = Business As Usual';
+comment on table intervention_maturity is 'what the maturity of the intervention is in this year; whether it is in the startup phase, being scaled up, or already operating and contuning business as ususal ';
+comment on column intervention_maturity.intervention_id is 'which intervention this maturity applies to';
+comment on column intervention_maturity.maturity is 'SU = Start Up. SC = Scale Up. BAU = Business As Usual';
+comment on column intervention_maturity.year is 'What year this maturity applies to';

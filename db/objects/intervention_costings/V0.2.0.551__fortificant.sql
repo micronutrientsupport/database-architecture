399abdfd342a7fc3297fd7e0d57319b1156d249d
Roman Roth <rroth@bgs.ac.uk>
2021-11-29 17:11:27 +0000

    remove extra spaces from fortificant names

--- db/objects/intervention_costings/V0.2.0.551__fortificant.sql
+++ db/objects/intervention_costings/V0.2.0.551__fortificant.sql
@@ -13,7 +13,7 @@ VALUES
     (80    , 'Zn'         , 'Zinc oxide'),
     (36    , 'Zn'         , 'Zinc sulfate'),
     (40    , 'Ca'         , 'Calcium carbonate'),
-    (7.5   , 'A'          , 'Retinyl Palmitate- 250 , 000 IU/g (dry)'),
+    (7.5   , 'A'          , 'Retinyl Palmitate- 250,000 IU/g (dry)'),
     (51.0  , 'A'          , 'Retinyl Palmitate- 1.7 m IU/g (oil)'),
     (81.0  , 'B1'         , 'Thiamin mononitrate'),
     (100.0 , 'B2'         , 'Riboflavin'),
@@ -22,7 +22,7 @@ VALUES
     (90    , 'Folic Acid' , 'Folic Acid'),
     (0.1   , 'B12'        , 'Vit. B-12 0.1% WS'),
     (3     , 'D'          , 'D-3 1.0 m IU/g (oil)'),
-    (0.3   , 'D'          , 'D-3  100 , 000 IU/g (dry)'),
+    (0.3   , 'D'          , 'D-3  100,000 IU/g (dry)'),
     (23.0  , 'E'          , 'all rac-a-tocopheryl acetate (dry)'),
     (59.0  , 'I'          , 'Potassium iodate'),
     (76.0  , 'I'          , 'Potassium iodide (for refined salt)')

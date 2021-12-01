2dcf5737dca732a327fb6f9db3f548e8d691ee18
Roman Roth <rroth@bgs.ac.uk>
2021-12-01 14:01:11 +0000

    add comments to intervention costings tables

--- db/objects/intervention_costings/V0.1.0.211__intervention_data.sql
+++ db/objects/intervention_costings/V0.1.0.211__intervention_data.sql
@@ -23,4 +23,12 @@ year_9              numeric,
 notes               text
 );
 
-COMMENT on table intervention_data is 'Table to store data directly loaded from excel spreadsheet costing models';
+COMMENT on table intervention_data is 'Table to store data directly loaded from excel spreadsheet intervetnion costing models';
+
+COMMENT ON COLUMN intervention_data.row_index     IS 'The row number of the data as it was in the excel spreadsheet e.g. cell B11 would have row_index = 11';
+COMMENT ON COLUMN intervention_data.row_name      IS 'a script-friendly name for a row that is the same for all food vehicles of a given intervetnion type e.g. Large-Scale Food Fortification (LSFF) will have "number_domestic_factories" for both wheat flour interventions and oil interventions, even though the factor_text is different ("Number of domestic industrial mills (capacity of > 20 MT/day)" vs. "Number of domstic industrial edible oil refineries (capacity of > 5 MT/day)") ';
+COMMENT ON COLUMN intervention_data.header1       IS 'The header/section to which this fields belongs. Relates directly to headings in the UI';
+COMMENT ON COLUMN intervention_data.header2       IS 'The subheader/subsection to which this fields belongs. Relates directly to subheadings in the UI';
+COMMENT ON COLUMN intervention_data.factor_text   IS 'The full text for this factor, as written in the excel sheet. Will be displayed like this in the UI';
+COMMENT ON COLUMN intervention_data.year_0        IS 'year_0 through year_9 contain the data values for the 10 years for this factor.';
+COMMENT ON COLUMN intervention_data.notes         IS 'Additional notes about the data e.g. assumptions and citation sources. Imported for completeness, will not be displayed in the UI';
--- db/objects/intervention_costings/V0.1.0.215__expected_loss.sql
+++ db/objects/intervention_costings/V0.1.0.215__expected_loss.sql
@@ -7,4 +7,6 @@ CREATE TABLE expected_loss(
 )
 ;
 
+COMMENT ON TABLE expected_loss IS 'The percentage of micronutrient that is expect to be lost between fortification and consumption e.g. some micronutrients deteriorate if exposed to heat on the way from the factory to the kitchen';
 
+COMMENT ON COLUMN expected_loss.expected_loss_percentage IS 'How much micronutrient is lost, in percent.';

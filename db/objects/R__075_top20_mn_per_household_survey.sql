51f3dd547b414f669f1a7c25ec25d8b520cb9110
Bean, Andrew K <andan@bsg.ac.uk>
2022-01-14 10:12:19 +0000

    chore: switch household table alias to hh

--- db/objects/R__075_top20_mn_per_household_survey.sql
+++ db/objects/R__075_top20_mn_per_household_survey.sql
@@ -17,12 +17,12 @@ FROM (
 		) as ranking
 	FROM (
 		SELECT
-			h.survey_id
+			hh.survey_id
 			, fi.fct_source_id
 			-- , cc.data_source_id
 			, mn.mn_name
 			, fi.food_genus_id
-			, sum( (mn.mn_value / 100 * amount_consumed_in_g) )/(select afe_total from survey_member_count smc where smc.survey_id = h.survey_id)  AS mn_consumed_per_day
+			, sum( (mn.mn_value / 100 * amount_consumed_in_g) )/(select afe_total from survey_member_count smc where smc.survey_id = hh.survey_id)  AS mn_consumed_per_day
 		FROM food_genus_nutrients fi
 		CROSS JOIN LATERAL (
 			VALUES
@@ -62,11 +62,11 @@ FROM (
 				('Moisture'    , Moisture_in_g              )
 		) AS mn("mn_name", "mn_value")
 		JOIN household_consumption hc ON hc.food_genus_id = fi.food_genus_id
-        JOIN household h ON hc.household_id = h.id
-        JOIN survey ON h.survey_id = survey.id
+        JOIN household hh ON hc.household_id = hh.id
+        JOIN survey ON hh.survey_id = survey.id
 		-- WHERE date_part('year', hc.date_consumed) = 2018
 		GROUP BY
-			h.survey_id
+			hh.survey_id
 			, fi.food_genus_id
 			, fi.fct_source_id
 			, mn_name

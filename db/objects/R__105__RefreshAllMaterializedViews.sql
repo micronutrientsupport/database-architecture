d661c85ef753e6e95300c2178377420017b9c1b7
Roman Roth <rroth@bgs.ac.uk>
2022-01-17 15:23:43 +0000

    fix RefreshAllMaterializedViews() not handling schema names with hyphens

--- db/objects/R__105__RefreshAllMaterializedViews.sql
+++ db/objects/R__105__RefreshAllMaterializedViews.sql
@@ -1,15 +1,19 @@
 CREATE OR REPLACE FUNCTION RefreshAllMaterializedViews(schema_arg TEXT)
-RETURNS INT AS $$
+RETURNS INT
+AS $$
 DECLARE
     r RECORD;
 BEGIN
     RAISE NOTICE 'Refreshing materialized view in schema %', schema_arg;
-    FOR r IN SELECT matviewname FROM pg_matviews WHERE schemaname = schema_arg 
+    FOR r IN
+    	SELECT matviewname, schemaname
+    	FROM pg_matviews
+    	WHERE schemaname = schema_arg
     LOOP
         RAISE NOTICE 'Refreshing %.%', schema_arg, r.matviewname;
-        EXECUTE 'REFRESH MATERIALIZED VIEW ' || schema_arg || '.' || r.matviewname; 
+        EXECUTE 'REFRESH MATERIALIZED VIEW ' || quote_ident(r.schemaname) || '.' || quote_ident(r.matviewname);
     END LOOP;
 
     RETURN 1;
-END 
+END
 $$ LANGUAGE plpgsql;

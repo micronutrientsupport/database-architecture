61821605e299cbeddcdeda912eb7edacb50d769c
Roman Roth <rroth@bgs.ac.uk>
2022-01-18 13:43:02 +0000

    add debugging logs to RefreshAllMaterializedViews()

--- db/objects/R__105__RefreshAllMaterializedViews.sql
+++ db/objects/R__105__RefreshAllMaterializedViews.sql
@@ -3,17 +3,27 @@ RETURNS INT
 AS $$
 DECLARE
     r RECORD;
+    starttime TIMESTAMP := clock_timestamp();
+    sectionstarttime TIMESTAMP := clock_timestamp();
 BEGIN
-    RAISE NOTICE 'Refreshing materialized view in schema %', schema_arg;
+    RAISE NOTICE 'Refreshing materialized views for schema % at %', quote_ident(schema_arg), current_time;
+
     FOR r IN
     	SELECT matviewname, schemaname
     	FROM pg_matviews
     	WHERE schemaname = schema_arg
+    	AND matviewname NOT IN ('household_deficiency_afe_aggregation') -- TODO: takes a long time
+    	ORDER BY matviewname
     LOOP
         RAISE NOTICE 'Refreshing %.%', schema_arg, r.matviewname;
-        EXECUTE 'REFRESH MATERIALIZED VIEW ' || quote_ident(r.schemaname) || '.' || quote_ident(r.matviewname);
+
+        EXECUTE 'REFRESH MATERIALIZED VIEW ' || quote_ident(r.schemaname) || '.' || r.matviewname;
+
+        RAISE NOTICE '% %', r.matviewname, lpad((clock_timestamp() - sectionstarttime)::text, 75 - length(r.matviewname));
+        sectionstarttime := clock_timestamp();
     END LOOP;
 
+    RAISE NOTICE 'Cumulative time spent is %', clock_timestamp() - starttime ;
     RETURN 1;
 END
 $$ LANGUAGE plpgsql;

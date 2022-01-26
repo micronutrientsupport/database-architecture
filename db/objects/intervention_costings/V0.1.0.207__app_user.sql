86535fd1cfc48c59659603546795b9936ac50189
Andy Bevan <apbe@bgs.ac.uk>
2022-01-26 09:26:32 +0000

    Add comments and unique constraint to app_user table

--- db/objects/intervention_costings/V0.1.0.207__app_user.sql
+++ db/objects/intervention_costings/V0.1.0.207__app_user.sql
@@ -1,6 +1,9 @@
 
 CREATE TABLE app_user(
-    id      text    PRIMARY KEY,
-    username    text
+    id          text    PRIMARY KEY,
+    username    text    UNIQUE
 )
 ;
+
+comment on table app_user is 'A table to record users of the application in order to assign ownership to e.g. intervention costong models';
+comment on column app_user.username is 'Unique username to represent the user - could be an email address.';

0d22783e088b76c0a630c42c5944b6f783f90ed2
Bean, Andrew K <andan@bsg.ac.uk>
2022-06-22 11:49:48 +0100

    fix: fix typo

--- db/objects/V0.1.0.756__country_fct_list.sql
+++ db/objects/V0.1.0.756__country_fct_list.sql
@@ -1,7 +1,7 @@
 	create table country_fct_list
 	(
 	id 	integer primary key references country_consumption_source (id),
-	fct_list 		text
+	fct_list 		text,
 	fct_list_id     integer,
 	FOREIGN KEY (fct_list_id) REFERENCES distinct_fct_list(id);
 	)

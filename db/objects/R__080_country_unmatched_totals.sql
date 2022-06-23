998c0186b533daab7c02c5dba9631ae06f4013d3
Bean, Andrew K <andan@bsg.ac.uk>
2022-06-23 10:53:55 +0100

    fix: update country unmatched

--- db/objects/R__080_country_unmatched_totals.sql
+++ db/objects/R__080_country_unmatched_totals.sql
@@ -4,15 +4,15 @@ CREATE OR REPLACE VIEW country_unmatched_totals AS
 with country_consumption_fct_matches as (
 select 
 data_source_id
-, 
-amount_consumed_in_g as amount_consumed_in_g
+, fct_list_id
+, amount_consumed_in_g as amount_consumed_in_g
 , cc.food_genus_id
 , ARRAY_AGG(distinct fct_list_id) as included_fcts
 from country_consumption cc 
 join fct_list_food_composition f on f.food_genus_id = cc.food_genus_id 
 group by 
 data_source_id, 
---fct_list_id, 
+fct_list_id, 
 cc.food_genus_id, 
 amount_consumed_in_g
 order by food_genus_id, data_source_id 
@@ -28,7 +28,7 @@ country_consumption_totals as (
 
 select 
 	cft.data_source_id as consumption_data_id
-	--, fct.id as composition_data_id
+	, fct_list_id as composition_data_id
 	, total_count as consumption_total_items
 	, count(amount_consumed_in_g) as consumption_unmatched_count
 	, round((count(amount_consumed_in_g)::numeric / total_count)*100,2) as consumption_unmatched_count_percentage
@@ -41,6 +41,7 @@ select
 	--join fct_source fct on fct.id != all (included_fcts)
 	join country_consumption_totals cct on cct.data_source_id = cft.data_source_id
 	group by cft.data_source_id, 
+	cft.fct_list_id
 	--fct.id,
 	cct.total_count, cct.total_weight --, amount_consumed_in_g, food_genus_id
 order by cft.data_source_id;--, composition_data_id;

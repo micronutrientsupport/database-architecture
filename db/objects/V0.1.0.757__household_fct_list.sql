1184c67e39e0b34809efadf5ff36f9622a4e2bbd
Bean, Andrew K <andan@bsg.ac.uk>
2022-07-15 15:41:53 +0100

    chore: fix typo

--- db/objects/V0.1.0.757__household_fct_list.sql
+++ db/objects/V0.1.0.757__household_fct_list.sql
@@ -3,7 +3,7 @@ create table household_fct_list
 household_id 	integer primary key references household (id),
 fct_list 		text,
 fct_list_id     integer,
-FOREIGN KEY (fct_list_id) REFERENCES distinct_fct_list(id);
+FOREIGN KEY (fct_list_id) REFERENCES distinct_fct_list(id)
 );
 
 COMMENT ON TABLE household_fct_list                       IS 'List of prioritised FCTs for a given household';

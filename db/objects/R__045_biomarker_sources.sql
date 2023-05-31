5e3cfb11b2bf50158789b3071b26c8bef5401468
Bean, Andrew K <andan@bsg.ac.uk>
2023-05-31 12:22:32 +0100

    feat: update biomarker datasources to include viable aggregation fields and other utility data

--- db/objects/R__045_biomarker_sources.sql
+++ db/objects/R__045_biomarker_sources.sql
@@ -4,8 +4,10 @@ with surveys as (
         survey.id as survey_id
         , the_biomarker_name as biomarker_name
         , the_biomarker_groups as group_id
+        , sra.reported_aggregations as aggregation_fields
         , micronutrient_id
         , survey.name as survey_name
+        , DATE_PART('Year', to_date(survey.surveying_date_end,'Mon-YY')) as survey_year
         , survey.description as survey_description
         , survey.geonetwork_uuid as survey_metadata_id
         , country.id as country_id
@@ -19,12 +21,13 @@ with surveys as (
 from country join survey on ST_COVERS(ST_ENVELOPE(survey.geometry), country.geometry)
 join survey_reported_biomarkers srb on survey.id=srb.survey_id 
 join survey_reported_groups srg on survey.id=srg.survey_id
+join survey_reported_aggregations sra on survey.id=sra.survey_id 
 CROSS  JOIN unnest (srb.reported_biomarkers) as the_biomarker_name
 CROSS  JOIN unnest (srg.reported_groups) as the_biomarker_groups
 join biomarker_micronutrient_mapping bmm on the_biomarker_name=bmm.biomarker_name
 where survey.survey_type = 'biomarker' 
 )
-select country_id, biomarker_name, group_id, micronutrient_id, survey_id, survey_name, survey_description, survey_metadata_id from surveys
+select country_id, biomarker_name, group_id, aggregation_fields, micronutrient_id, survey_id, survey_name, survey_year, survey_description, survey_metadata_id from surveys
 where rank = 1;
 
 COMMENT ON VIEW biomarker_data_sources IS 'View of simplified algorithm for "best" biomarker data source for a given country';

b0950c7cb579639fe0b5c8fcd3d12f44b4c3b6b8
Roman Roth <rroth@bgs.ac.uk>
2023-06-07 15:18:40 +0100

    add more columns to intervention_cell_formula
    in order to accommodate more than the first 4 years

--- db/objects/intervention_costings/V0.1.0.217__intervention_cell_formula.sql
+++ db/objects/intervention_costings/V0.1.0.217__intervention_cell_formula.sql
@@ -12,7 +12,13 @@ cell_formula_0        text,
 cell_formula_1        text,
 cell_formula_2        text,
 cell_formula_3        text,
-cell_formula_4        text
+cell_formula_4        text,
+cell_formula_5        text,
+cell_formula_6        text,
+cell_formula_7        text,
+cell_formula_8        text,
+cell_formula_9        text,
+cell_formula_10       text
 );
 
 COMMENT on table intervention_cell_formula is 'Table to store cell formulae loaded from excel spreadsheet costing models';

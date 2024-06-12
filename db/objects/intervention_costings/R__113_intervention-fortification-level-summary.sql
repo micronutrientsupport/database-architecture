1dfe8274ccc1350f900ee00cdc815d32784a2cbd
Andrew Bean <39411049+bgsandan@users.noreply.github.com>
2024-06-12 13:16:51 +0100

    Release to main (#429)
    * fix: various intervention fixes
    
    * feat: premix calculator updateS:
    
    * chore(develop): release 1.4.0
    
    * chore: fix declaration order
    
    * chore(develop): release 1.4.1
    
    * chore: fix declaration order
    
    * chore: remove unneccesary files
    
    * chore(develop): release 1.4.2
    
    * fix: add missing updates
    
    * chore(develop): release 1.4.3
    
    * fix: source field for intervention data
    
    * chore(develop): release 1.4.4
    
    * feat: revamp matching to use static intake table (externally populated)
    
    * fix: household intake data table rather than view
    
    * fix: update fct match
    
    * chore(develop): release 1.5.0
    
    * fix: fix typo
    
    * fix: fix typo
    
    * fix: fix typo
    
    * fix: fix typo
    
    * fix: fix typo
    
    * fix: fix typo
    
    * fix: match function tweak
    
    * fix: match function tweak
    
    * chore(develop): release 1.5.1 (#417)
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * fix: tweak household matching, intervention data fixes (#422)
    
    Co-authored-by: Bean, Andrew K <andan@bsg.ac.uk>
    
    * Andan intervention formula susu recurring (#424)
    
    * fix: tweak household matching, intervention data fixes
    
    * fix: tweak
    
    ---------
    
    Co-authored-by: Bean, Andrew K <andan@bsg.ac.uk>
    
    * Andan intervention formula susu recurring (#425)
    
    * fix: tweak household matching, intervention data fixes
    
    * fix: tweak
    
    * fix: try to fix country view stats
    
    ---------
    
    Co-authored-by: Bean, Andrew K <andan@bsg.ac.uk>
    
    * Andan intervention formula susu recurring (#426)
    
    * fix: tweak household matching, intervention data fixes
    
    * fix: tweak
    
    * fix: try to fix country view stats
    
    * revert: undo changes to fct matcj fn
    
    ---------
    
    Co-authored-by: Bean, Andrew K <andan@bsg.ac.uk>
    
    * Update V0.2.0.804__match_fct_micronutrients.sql
    
    * chore(develop): release 1.5.2 (#423)
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * Andan intervention user cost (#428)
    
    * feat: add table to store user entred costs
    
    * chore: sort filename issues
    
    * chore: fix tpyo
    
    * chore: fix col name in check
    
    * feat: add tables for subnational population, intervention specific thresholds and associated handlers and triggers
    
    * chore: fix import order
    
    * db/objects/intervention_costings
    
    * fix: only trigger CND calculation on intervention-threshold table
    
    * chore: typo
    
    * feat: add expected losses tables
    
    * chore: fix filenames to ensure flyway picks up all migrations
    
    * chore: remove fortifiable foods dummy data import
    
    * feat: add expected loss and summary fortification level views
    
    ---------
    
    Co-authored-by: Bean, Andrew K <andan@bsg.ac.uk>
    
    ---------
    
    Co-authored-by: Bean, Andrew K <andan@bsg.ac.uk>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ db/objects/intervention_costings/R__113_intervention-fortification-level-summary.sql
@@ -0,0 +1,74 @@
+create or replace view intervention_fortification_level_summary as
+
+with fortificant_targets as (
+select 
+	intervention_id, 
+	((unnest(food_vehicle_standard))->>'micronutrient')::text as micronutrient_id, 
+	((json_array_elements((unnest(food_vehicle_standard))->'compounds'))->>'targetVal')::numeric as target_val
+from intervention_vehicle_standard ivs
+)
+,
+standards as (
+select
+	intervention_id,
+	micronutrient_id,
+	max(target_val) as standard
+from fortificant_targets
+group by intervention_id, micronutrient_id
+),
+fortifiable as (
+select * 
+from intervention_data id 
+where header1 = 'Program assumptions' 
+	and header2 = 'Performance over time' 
+	and row_name = 'fortifiable_vehicle_pct'
+),
+fortified as (
+select * 
+from intervention_data id 
+where header1 = 'Program assumptions' 
+	and header2 = 'Performance over time' 
+	and row_name = 'fortified_fortifiable_vehicle_pct'
+),
+average as (
+select * 
+from intervention_data id 
+where header1 = 'Program assumptions' 
+	and header2 = 'Performance over time' 
+	and row_name = 'avg_level_fortified_fortifiable_vehicle_pct'
+),
+baseline as (
+select
+   fortifiable.intervention_id
+   , unnest(array['2021', '2022', '2023','2024']) AS "Year"
+   , intervention.food_vehicle_id as fortification_vehicle_id
+   , food_vehicle.vehicle_name as fortification_vehicle_name
+   , unnest(array[fortifiable.year_0, fortifiable.year_1, fortifiable.year_2,fortifiable.year_3]) AS "perc_fortifiable"
+   , unnest(array[fortified.year_0, fortified.year_1, fortified.year_2,fortified.year_3]) AS "perc_fortified"
+   , unnest(array[average.year_0, average.year_1, average.year_2,average.year_3]) AS "perc_average_fortification_level"
+from 
+	fortifiable 
+		join fortified on fortifiable.intervention_id = fortified.intervention_id
+		join average on fortifiable.intervention_id = average.intervention_id
+		join intervention on intervention.id = fortifiable.intervention_id
+		join food_vehicle on food_vehicle.id = intervention.food_vehicle_id 
+order by intervention_id, "Year"
+)
+
+select 
+	b.*
+	, (select standard from standards s where s.intervention_id=b.intervention_id and s.micronutrient_id = 'A' ) 
+		* b.perc_fortifiable * b.perc_fortified * b.	perc_average_fortification_level as "A"
+	, (select standard from standards s where s.intervention_id=b.intervention_id and s.micronutrient_id = 'B12' ) 
+		* b.perc_fortifiable * b.perc_fortified * b.	perc_average_fortification_level as "B12"
+	, (select standard from standards s where s.intervention_id=b.intervention_id and s.micronutrient_id = 'B2' ) 
+		* b.perc_fortifiable * b.perc_fortified * b.	perc_average_fortification_level as "B2"
+	, (select standard from standards s where s.intervention_id=b.intervention_id and s.micronutrient_id = 'B3' ) 
+		* b.perc_fortifiable * b.perc_fortified * b.	perc_average_fortification_level as "B3"
+	, (select standard from standards s where s.intervention_id=b.intervention_id and s.micronutrient_id = 'B9' ) 
+		* b.perc_fortifiable * b.perc_fortified * b.	perc_average_fortification_level as "B9"
+	, (select standard from standards s where s.intervention_id=b.intervention_id and s.micronutrient_id = 'Fe' ) 
+		* b.perc_fortifiable * b.perc_fortified * b.	perc_average_fortification_level as "Fe"
+	, (select standard from standards s where s.intervention_id=b.intervention_id and s.micronutrient_id = 'Zn' ) 
+		* b.perc_fortifiable * b.perc_fortified * b.	perc_average_fortification_level as "Zn"
+from baseline b;

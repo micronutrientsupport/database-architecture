1a9e1b5a8c5b0068f258bfb93e4994709978c230
Bean, Andrew K <andan@bsg.ac.uk>
2024-12-14 16:59:43 +0000

    feat: af targeting updates

--- db/objects/intervention_costings/R__118_farmer_adoption_rates.sql
+++ db/objects/intervention_costings/R__118_farmer_adoption_rates.sql
@@ -1,7 +1,5 @@
 create or replace view intervention_farmer_adoption_rates as
 
-
-
 with baseline_assumptions as 
 (
 select 
@@ -47,3 +45,53 @@ from
     adoption_local al
     join adoption_hybrid ah on al.intervention_id = ah.intervention_id
     join adoption_recycled ar on al.intervention_id = ar.intervention_id;
+
+
+
+
+create or replace view intervention_farmer_adoption_rates_af as
+with baseline_assumptions as 
+(
+select 
+	intervention_id
+	, json_array_elements(data)->>'rowName' as row_name
+	, json_array_elements(data) as data
+from intervention_values_json ivj
+where header1 = 'Farmer adoption rates'
+),
+adoption_overall AS (
+	select
+		intervention_id
+	   	, data as adoption_rate_overall
+	from baseline_assumptions ba
+	where ba.row_name = 'farmer_agro_biofort_adoption_rate_overall'
+),
+adoption_granular as (
+	select
+		intervention_id
+	   	, data as adoption_rate_granular
+	from baseline_assumptions ba
+	where ba.row_name = 'farmer_adoption_rate_among_adopters_granular'
+),
+adoption_foliar as (
+	select
+		intervention_id
+	   	, data as adoption_rate_foliar
+	from baseline_assumptions ba
+	where ba.row_name = 'farmer_adoption_rate_among_adopters_foliar'
+)
+
+select
+    al.intervention_id,
+    json_build_object(
+        'adoptionRateOverall',
+        adoption_rate_overall,
+        'adoptionRateGranular',
+        adoption_rate_granular,
+        'adoptionRateFoliar',
+        adoption_rate_foliar
+	) as farmer_adoption_rates
+from
+    adoption_overall al
+    join adoption_granular ah on al.intervention_id = ah.intervention_id
+    join adoption_foliar ar on al.intervention_id = ar.intervention_id;
